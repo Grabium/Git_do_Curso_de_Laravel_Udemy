0756657538347b05f237363cd0113eb33e0bd2de
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2023-05-23 14:05:51 -0300

    InicioLaravel_InsContr.php
    Mais detalhes sobre as classes de controle tendo como exemplo a classe "InicioLaravel_InsContr".

--- Projeto/app/Http/Controllers/InicioLaravel_InsContr.php
+++ Projeto/app/Http/Controllers/InicioLaravel_InsContr.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 //use Illuminate\Http\Request;
 
+//com a class importada pelo "namespace App\Http\Controllers;" neste arquivo.
+//uma rota (em routes/web.php) pode importar esta 
+//classe instanciada abaixo "class InicioLaravel_InsContr extends Controller"
+//e no callback da rota, chamar a classe e esta função "loadPageInicioLaravel()"
+//esta função, por sua vez, buscar o recurso front-end direto 
+//da pasta resources/view com a função "view()".
+
 class InicioLaravel_InsContr extends Controller
 {
     public function loadPageInicioLaravel(){
